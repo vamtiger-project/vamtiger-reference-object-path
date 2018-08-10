027432f0cc8d23a7c99b6b873589dc80a2ba3128
James-Alex Matthews <vamtiger@vamtiger.com>
2018-08-10 22:04:10 +0200

    vamtiger-node-typescript-commit

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,14 +1,9 @@
-/// <reference path="../node_modules/vamtiger-regex-period/build/index.d.ts" />
-/// <amd-module name="vamtiger-reference-object-path" />
-declare module "vamtiger-reference-object-path" {
-    const _default: ({ object, path: objectPath }: Params) => any;
-    export default _default;
-    export interface Params {
-        object: IObject;
-        path: string;
-    }
-    export interface IObject {
-        [key: string]: any;
-    }
+declare const _default: ({ object, path: objectPath }: Params) => any;
+export default _default;
+export interface Params {
+    object: IObject;
+    path: string;
 }
-declare module "test/01-reference-object-path.spec" { }
+export interface IObject {
+    [key: string]: any;
+}
